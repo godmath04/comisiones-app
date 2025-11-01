92ebf921c1d922c6c90261a0a4428468c171b28c
LuisPineda <luis.pineda.revelo@udla.edu.ec>
2025-10-31 21:54:19 -0500

    Conexion a azure

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: false,
-  apiBaseUrl: 'https://localhost:7271'
+  apiBaseUrl: 'https://apicorecomisiones20251031214507-e3e4dnashbcuebe4.canadacentral-01.azurewebsites.net/'
 }
